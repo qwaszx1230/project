40ce16be898fd7607ec4dbbed14d0eab35359005
547519233@qq.com <547519233@qq.com>
2016-12-22 10:39:11 +0800

    删除

--- qiushiAPP/www/js/js.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('#popColse').click(function(event) {
-		console.log('111')
-	});
-	// $('.popBox .popColse').on('click',function(event) {
-	// 	event.preventDefault();
-	// 	console.log('111')
-	// });
-});
